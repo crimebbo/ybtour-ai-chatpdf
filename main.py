9c1f3330e8d7852b0c943b4d47a226b532a890fe
crimebbo <crimebbo@naver.com>
2024-02-19 14:29:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -132,8 +132,9 @@ from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
+from streamlit_extras.buy_me_a_coffee import button
 
-
+button(username="ybtour", floating=True, width=221)
 #제목
 st.title("ChatPDF")
 st.write("---")
